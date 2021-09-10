4ffbe4b5ce47f63bd44f2c50b642e25b7f8b8719
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-09 18:30:37 -0600

    create function for password length with errors

--- Develop/script.js
+++ Develop/script.js
@@ -25,18 +25,26 @@ function randomSpecialCharacters(){
   return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 }
 
-console.log(randomSpecialCharacters()); //checking function works correctly
-
 // Write password to the #password input
 var writePassword = function (){
-  window.prompt("Please select a password length between 8-128 characters:");
-}; if(aNumber == NaN || aNumber < 8 || aNumber > 128 || aNumber == ""){
-window.prompt("You may only enter a number between 8-128. \nPlease try again:");
-} else {
-  alert("Woo!");
-};
+  var userInput = Number(window.prompt("Please select a password length between 8-128 characters:")); //changes from typeof string to typeof #
+    userInput = Math.floor(userInput)
+    console.log(userInput)
+      if (isNaN(userInput) || userInput < 8 || userInput > 128 || userInput == "") {
+            alert("You may only enter a number between 8-128. \nPlease rethink your life choices and try again.");
+      } else { //will put out a boolean
+     var hasSpecialCharacters = window.confirm("Do you want special characters?");
+     var hasLowerCase = window.confirm("Do you want lower case letters?");
+     var hasUpperCase = window.confirm("Do you want upper case letters?");
+     var hasNumbers = window.confirm("Do you want numbers?");
+         if (hasSpecialCharacters === false && hasLowerCase === false && hasUpperCase === false && hasNumbers === false){
+           alert("DUDE, really???");
+         }
+      }
+    };
+
  
-//   var password = generatePassword(); //WHAT'S THIS? - why local?
+//   var password = generatePassword(); //WHAT'S THIS? - why local? everything gets written
 //   var passwordText = document.querySelector("#password"); //This refers back to the index ID, where psswd written?
 
 //   passwordText.value = password; //what's this?
