1e257ab12c8c08379357333bfe4797a432d0ac6b
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-10 11:41:03 -0600

    Simplifying boolean function for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -6,14 +6,12 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-var aNumber = [8-128];
-//Creating object
-const randomPassword = {
-  lower: randomLower,
-  upper: randomUpper,
-  number: randomNumber,
-  symbol: randomSpecialCharacters
-};
+
+//Setting global boolean to false until user selects "true"
+var hasLowerCase = false;
+var hasUpperCase = false;
+var hasSpecialCharacters = false;
+var hasNumbers = false;
 
 // Creating function that generates random characters
 function randomLower(){
@@ -35,55 +33,31 @@ function randomSpecialCharacters(){
 // Write password to the #password input
 var writePassword = function (){
   var userInput = Number(window.prompt("Please select a password length between 8-128 characters:")); //changes from typeof string to typeof #
-    userInput = Math.floor(userInput)
-    console.log(userInput)
-      if (isNaN(userInput) || userInput < 8 || userInput > 128 || userInput == "") {
+    userInput = Math.floor(userInput);
+      while (isNaN(userInput) || userInput < 8 || userInput > 128 || userInput == "") {
             alert("You may only enter a number between 8-128. \nPlease rethink your life choices and try again.");
-      } else { //will put out a boolean
-     var hasSpecialCharacters = window.confirm("Do you want special characters?");
-     var hasLowerCase = window.confirm("Do you want lower case letters?");
-     var hasUpperCase = window.confirm("Do you want upper case letters?");
-     var hasNumbers = window.confirm("Do you want numbers?");
+      }
+//create password based of user's userInput(length) and characters selected
+      if (userInput) { 
+     if (window.confirm("Do you want special characters?") === true){
+       hasSpecialCharacters = true
+     }
+
+     if(window.confirm("Do you want lower case letters?") === true){
+       hasLowerCase = true
+     }
+     if(window.confirm("Do you want upper case letters?") ===true){
+       hasUpperCase = true
+     }
+     if(window.confirm("Do you want numbers?") === true){
+       hasNumbers = true
+     }
+  // if user picks all false, kick out error/try again msg
          if (hasSpecialCharacters === false && hasLowerCase === false && hasUpperCase === false && hasNumbers === false){
            alert("Hm...let's try that again, remember to pick at least one character next time.");
          }
-         else if (hasSpecialCharacters === true && hasNumbers === true && hasUpperCase === true && hasLowerCase === true){
-           alert("Cool, you want everything");
-         }
-         else if (hasSpecialCharacters === true && hasNumbers === true && hasUpperCase === true){
-          alert("Cool,so no lowercase");
-        }
-        else if (hasSpecialCharacters === true && hasNumbers === true && hasLowerCase === true){
-          alert("Cool, so no uppercase");
-        }
-        else if (hasSpecialCharacters === true && hasUpperCase === true && hasLowerCase === true){
-          alert("Cool, so no numbers");
-        }
-        else if (hasNumbers === true && hasUpperCase === true && hasLowerCase === true){
-          alert("Cool, so no symbols");
-        }
-        else if (hasSpecialCharacters === true && hasNumbers === true){
-          alert("Cool, so no letters");
-        }
-        else if (hasUpperCase === true && hasLowerCase === true){
-          alert("Cool, only letters");
-        }
-        else if (hasSpecialCharacters === true && hasLowerCase === true){
-          alert("Cool, symbols and lowercase");
-        }
-        else if (hasNumbers === true && hasLowerCase === true){
-          alert("Cool, numbers and lowercase");
-        }
-        else if (hasSpecialCharacters === true && hasUpperCase === true ){
-          alert("Cool, symbols and uppercase");
-        }
-        else if (hasNumbers === true && hasUpperCase === true){
-          alert("Cool, numbers and uppercase");
-        }
       }
     };
-
-// how do i use the true boolean to grab it... i don't want to create a crap ton of functions
 
 
 //  function iDk(){
