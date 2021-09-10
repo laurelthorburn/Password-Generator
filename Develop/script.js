fd3ca685eebe9b79444e4345a779efeb309e85fd
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-10 16:57:37 -0600

    Changing user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,7 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+//Creating global variable for final password
 var userPassword = function generatePassword(){
   var allCharacters = [];
   var finalPassword = '';
@@ -17,16 +18,16 @@ var userPassword = function generatePassword(){
             alert("You may only enter a number between 8-128. \nPlease rethink your life choices and try again.");
         }
         else {
-              if(window.confirm("Do you want special characters?")){
+              if(window.confirm("Do you want special characters? If so, click 'OK'.")){
                 Array.prototype.push.apply(allCharacters, specialCharacters);
               }
-              if(window.confirm("Do you want numbers?")){
+              if(window.confirm("Do you want numbers? If so, click 'OK'.")){
                 Array.prototype.push.apply(allCharacters, numbers);
               }
-              if(window.confirm("Do you want lowercase letters?")){
+              if(window.confirm("Do you want lowercase letters? If so, click 'OK'." )){
                 Array.prototype.push.apply(allCharacters, lowerCase);
               }
-              if(window.confirm("Do you want uppercase letters?")){
+              if(window.confirm("Do you want uppercase letters? If so, click 'OK'.")){
                 Array.prototype.push.apply(allCharacters, upperCase);
               }
 // if user picks all false, kick out error/try again msg
